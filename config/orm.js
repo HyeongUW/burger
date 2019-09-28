28977336e4b5c0e751e4a4e7c8143bbe410711bc
Hyeong Suk Kim <whddkf2004@gmail.com>
2019-09-27 18:53:57 -0700

    working on server.js, burger_controller.js, burger.js, orm.js

--- config/orm.js
+++ config/orm.js
@@ -98,5 +98,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the model (burger.js).
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-// server.js -> catsController.js -> cat.js -> orm.js
+// server.js -> burger_Controller.js -> burger.js -> orm.js
 
 var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
+
+// Serve static content for the app from the "public" 
+// directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs( { defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+// Start our server so that it can begin listening 
+// to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
